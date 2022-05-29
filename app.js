aea627a0c7c3fdf353ea27cb7c9d4b3db581f01c
wajeehuddin158 <106189078+wajeehuddin158@users.noreply.github.com>
2022-05-29 09:17:17 +0500

    4 chapters completed

---
+++ app.js
@@ -0,0 +1,110 @@
+// //      ASSIGNMENT 1
+// //      CHAPTER 1
+// //    Q:1
+// alert("Greetings User")
+
+// // Q:2
+// alert("Error! please enter a correct password.")
+
+// // Q:3
+// alert("Welcome to JS Land...\n  Happy Coding!")
+
+// // Q:4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// //      CHAPTER 2
+// //    Q:1
+//  var username;
+
+//  //    Q:2
+//  var myName = "Wajeeh Uddin Farooqui"
+
+//  //    Q:3
+//  var message = "Hello World"
+//     alert( message)
+
+// //    Q:4
+// var stdName = "john cena"
+// var stdAge = 19
+// var stdCourse = "Web and Mobile App Development"
+// alert(stdName)
+// alert(stdAge)
+// alert(stdCourse)
+
+// //    Q:5
+// var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza)
+
+// //    Q:6
+// var email = "wajeehuddin158@gmail.com"
+// alert("My Email address is " + email)
+
+// //    Q:7
+// var book = "A Smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+// //    Q:8
+// var js = "Javascript"
+// document.write("Yah! I can write HTML content through " + js)
+
+// //    Q:9
+// var end = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(end)
+
+// //      ASSIGNMENT 2
+// //      CHAPTER 3
+// //    Q:1   
+// var age = 20
+// alert("My age is " + (age) + " years" )
+
+// //    Q:2
+// var sitevisits = 14
+// alert("You have visited this site " + sitevisits + " times")
+
+// //   Q:3
+// var birthYear = 2001
+// document.write(("My Birthyear is ") + birthYear + ("</br>") + "Data type of my declared variable is " + typeof (birthYear) )
+
+// //    Q:4
+// var visitorname = "Bhutto Jani"
+// var producttitle = "T-shirts"
+// var productqty = 5
+// document.write( visitorname  + " ordered " +  productqty + " " + producttitle + " from XYZ clothing Store" )
+
+// //      CHAPTER 4
+// //    Q:1
+// var myName,myAge,mySalary ;
+
+// //    Q:2
+// // LEGAL VARIABLES
+// var myname = "WAJEEH UDDIN FAROOQUI";
+// var myName = "WAJEEH UDDIN FAROOQUI";
+// var my_name = "WAJEEH UDDIN FAROOQUI";
+// var _my_name = "WAJEEH UDDIN FAROOQUI";
+// var $my_name = "WAJEEH UDDIN FAROOQUI";
+
+// // ILLEGAL VARIABLES
+// var @myname = "WAJEEH UDDIN FAROOQUI";
+// var 123 = "WAJEEH UDDIN FAROOQUI";
+// var 123myname = "WAJEEH UDDIN FAROOQUI";
+// var my name = "WAJEEH UDDIN FAROOQUI";
+// var my&name = "WAJEEH UDDIN FAROOQUI";
+
+// //    Q:3
+// document.write(" <h1> Rules For Naming JS Variables </h1>  <hr> <br>")
+// document.write("<h4>  1)Variable names can only contain _________, _________, ________ and ________.</h4> <br>")
+// document.write("<h4>  2)Variables must begin with a ________, ________ or ________ </h4> <br>")
+// document.write("<h4>  3)Variable names are case _________.</h4> <br>")
+// document.write("<h4>  4)Variable names should not be JS_________.</h4> <br>")
+
+// document.write("<h3> ANSWERS </h3> <hr>")
+// document.write(" <h4> 1)<b>Letter, Number, $ and _  .</b> </h4>")
+// document.write(" <h4> 2)<b>Letter, $, _  .</b> </h4>")
+// document.write(" <h4> 3)<b>Case sensitive.</b> </h4>")
+// document.write(" <h4> 4)<b> Keywords.</b> </h4>")
+
+// //      CHAPTER 5
+//    Q:1
+
+
